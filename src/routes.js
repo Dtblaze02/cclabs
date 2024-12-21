18a805df433d3f42a4fd480ef3daeed04db169d4
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-12-21 03:12:06 +0100

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    name: "About",
+    icon: <Icon>info</Icon>,
+    route: "about",
+  },
+  {
+    name: "Features",
+    icon: <Icon>fingerprintRounded</Icon>,
+    route: "features",
+  },
+  {
+    name: "Benefits",
+    icon: <Icon>pageview</Icon>,
+    route: "benefits",
+  },
+];
+
+export default routes;
