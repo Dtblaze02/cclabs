29516f609c8f7328764947b8152b8c305578265d
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-12-21 02:56:36 +0100

    Create textField.js

---
+++ src/assets/theme/components/form/textField.js
@@ -0,0 +1,13 @@
+
+//  React Base Styles
+import colors from "assets/theme/base/colors";
+
+const { transparent } = colors;
+
+export default {
+  styleOverrides: {
+    root: {
+      backgroundColor: transparent.main,
+    },
+  },
+};
