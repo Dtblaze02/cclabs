1e282e10cc103daaee5e49bc0fa22645fbe2a543
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-12-21 03:11:44 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const key_features = [
+  { id: "1", title: "Profile", icon: "person", description: "Signup/Login" },
+  {
+    id: "2",
+    title: "Resourse Library",
+    icon: "library_books",
+    description: "Access exclusive tutorials, webinars and workshops",
+  },
+  { id: "3", title: "Chat", icon: "chat", description: "Connect with like minds" },
+  {
+    id: "4",
+    title: "Project Showcase",
+    icon: "diversity_2",
+    description: "Highlight your work and get discovered by industry leaders and peers.",
+  },
+  {
+    id: "5",
+    title: "Collaboration Hub",
+    icon: "hub",
+    description: "Find talented Believers to partner on faith-based problem-solving projects.",
+  },
+  {
+    id: "6",
+    title: "Mentorship Programs",
+    icon: "leaderboard",
+    description: `Learn from experienced Christian professionals in faith-based creative 
+    industries.`,
+  },
+  {
+    id: "7",
+    title: "Event Calendar",
+    icon: "event-calendar",
+    description: "Stay updated on our meet-ups, conferences, and workshops.",
+  },
+];
